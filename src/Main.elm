0102e3c119e4fd299108f18c6eca019910fdf55f
Iruyan_Zak <yuri.kazuna@gmail.com>
2018-06-21 18:22:26 +0900

    hoge

--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,43 @@
 module Main exposing (..)
 
 import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html.Attributes exposing (class)
+import Dict
+import Dict exposing (Dict)
 
 
 ---- MODEL ----
 
+type Color
+    = Black
+    | White
 
-type alias Model =
-    {}
+
+type alias Position
+    = (Int, Int)
+
+type alias Board
+    = Dict Position (Maybe Color)
+
+type alias Model
+    = { board : Board
+      , turn : Color
+      }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
-
+    let
+        emptyBoard
+            = product (List.range 1 8) (List.range 1 8)
+            |> List.map (\p -> (p, Nothing))
+            |> Dict.fromList
+        initialStones
+            = [((4, 4), Just Black), ((4, 5), Just White), ((5, 4), Just White), ((5, 5), Just Black)]
+            |> Dict.fromList
+        board = Dict.union initialStones emptyBoard
+    in
+        ( { board = board, turn = Black }, Cmd.none )
 
 
 ---- UPDATE ----
@@ -22,6 +45,7 @@ init =
 
 type Msg
     = NoOp
+    | Click Position
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -34,11 +58,9 @@ update msg model =
 
 
 view : Model -> Html Msg
-view model =
-    div []
-        [ img [ src "./src/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
-        ]
+view { board } =
+    div [class "board"]
+        <| Dict.foldr (\_ cell seq -> dispCell cell :: seq) [] board
 
 
 
@@ -53,3 +75,56 @@ main =
         , update = update
         , subscriptions = always Sub.none
         }
+
+
+product : List a -> List b -> List (a, b)
+product xs ys =
+    List.map (\x -> List.map (\y -> (x,y)) ys) xs
+        |> List.concat
+
+
+dispCell : Maybe Color -> Html Msg
+dispCell stone =
+    case stone of
+        Nothing  -> div [class "cell"] []
+        (Just s) -> div [class <| "cell"]
+                    [ div [class <| String.toLower <| toString s] []
+                    ]
+
+mapMaybe : (a -> Maybe b) -> List a -> List b
+mapMaybe func seq =
+    List.filter (\e -> e /= Nothing) <| List.map func seq
+
+search : Color -> Board -> Dict Position (List Position)
+search color board =
+    let
+        bases = board
+              |> Dict.filter (\_ s -> s == Just color)
+              |> Dict.keys
+
+        directions = product [-1, 0, 1] [-1, 0, 1]
+
+        candidates = flip List.map directions
+                     <| \pos -> mapMaybe (search' board [] color pos) bases
+
+
+search' : Board -> List Position -> Color -> Position -> Position -> Maybe (Position, List Position)
+search' board flippables edgeColor (dx, dy) (baseX, baseY) =
+    let
+        nextPos = (baseX + dx, baseY + dy)
+        nextCell = Dict.get nextPos board
+
+        (finish, flippables') =
+            case nextCell of
+                Nothing -> (True, [])  -- board上にないセル、つまり枠外 ==> 置けない
+                Just color -> (True, [])  -- 同じ色の石で塞いである ==> 置けない
+                Just Nothing -> (True, flippables)  -- 空きセル ==> ひっくり返せる石が1個でもあれば置ける
+                _ -> (False, nextPos :: flippables)  -- 反対の色の石がある ==> ひっくり返せる石に加えて次の石を見る
+    in
+        if finish then
+            if List.isEmpty flippables' then
+                Nothing
+            else
+                Just (nextPos, flippables')
+        else
+            search' board flippables' edgeColor (dx, dy) nextPos
